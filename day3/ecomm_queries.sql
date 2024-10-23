9dc1b803c208677281a07ad2e67a33f717020a53
lakshmantutike <lakshmantutike>
2024-10-23 02:42:31 +0000

    commit on 23rd Oct

---
+++ day3/ecomm_queries.sql
@@ -0,0 +1,10 @@
+-- Databricks notebook source
+select * from laks_training_dev.ecomm_dlt.sales_bronze
+
+-- COMMAND ----------
+
+select * from laks_training_dev.ecomm_dlt.sales_silver
+
+-- COMMAND ----------
+
+select * from laks_training_dev.ecomm_dlt.sales_bronze
